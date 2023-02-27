10da48f5c5891a2b5cc8f4a021c17860f84ecfbe
vipbhzz <99421781+vipbhzz@users.noreply.github.com>
2023-02-27 16:48:17 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+    <?php
+    echo"Ola mundo";
+    ?>
+    </h1>
+
+    <h2>
+    <?php
+    echo (1+1);
+     ?>
+    </h2>
+
+    <h2>
+    <?php
+    echo (3-1);
+    ?>
+    </h2>
+
+    <h2>
+    <?php
+    echo (2*1);
+    ?>
+    </h2>
+
+    <h2>
+     <?php
+     echo (2+2);
+     ?>
+    </h2>
+</body>
+</html>
